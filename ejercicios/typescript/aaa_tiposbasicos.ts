b310db10a134c398f01d16065d808c26fb2376c7
Juan Sáez García <contacto@juamber.com>
2022-01-05 17:23:18 +0100

    aab_tiposbasicos

---
+++ ejercicios/typescript/aaa_tiposbasicos.ts
@@ -0,0 +1,26 @@
+let muted: boolean = true;
+let numerador: number = 42;
+let donominador: number = 6;
+let resultado = numerador / donominador;
+
+let nombre: string = "Ricahrd";
+let saludo = `Me llamo ${nombre}`;
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push(8);
+peopleAndNumbers.push("Juan");
+
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul"
+}
+
+let colorFav: Color = Color.Azul;
+console.log(colorFav);
+
+let comodin: any = "Jocker";
+comodin = { age: 15 };
+
